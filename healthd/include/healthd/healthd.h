0c5455b496af99aca13c6b54b76265f7c78a1411
Vachounet <vachounet@live.fr>
2018-12-08 12:38:27 +0100

    core: update battery mod support for P
    Change-Id: I6fa06cdb9f95c50f9d5e994450776472148c05a3
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>
    Signed-off-by: klozz <shaclone707@gmail.com>

--- healthd/include/healthd/healthd.h
+++ healthd/include/healthd/healthd.h
@@ -67,15 +67,6 @@ struct healthd_config {
     android::String8 batteryChargeCounterPath;
     android::String8 batteryFullChargePath;
     android::String8 batteryCycleCountPath;
-	//Initial mods things
-    android::String8 modChargeFullPath;
-    android::String8 modCapacityPath;
-    android::String8 modStatusPath;
-    android::String8 modTypePath;
-    android::String8 modPowerSourcePath;
-    android::String8 modRechargeStartLevelPath;
-    android::String8 modRechargeStopLevelPath;
-	//end mods things
 
     int (*energyCounter)(int64_t *);
     int boot_min_cap;
