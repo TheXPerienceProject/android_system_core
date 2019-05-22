MotorolaMobilityLLC/system-core

storaged: fixed the ap panic when system rebooot.
For storaged serivce need some other services support, like
batteryproperties service, when system do reboot, the batteryproperties is exit
before storaged. so may caused the storaged crash.
crash log:

01-31 23:17:39.091 1120 1120 E /system/bin/storaged: batteryproperties service died, exiting
01-31 23:17:39.092 1120 6821 W IPCThreadState: Calling IPCThreadState::self() during shutdown is dangerous, expect a crash.
--------- beginning of crash
01-31 23:17:39.097 1120 6821 F libc : Fatal signal 11 (SIGSEGV), code 1, fault addr 0xe0 in tid 6821 (Binder:1120_2), pid 1120 (storaged)
01-31 23:17:39.109 648 648 I ServiceManager: service 'storaged' died

so we add a protection that when system reboot, make storaged just wait reboot, not call stop and exit.

QC case: 03339157

Change-Id: I2c72db263cb5582cb1a77e4ed770bb619d7f0025
Reviewed-on: https://gerrit.mot.com/1132257
SME-Granted: SME Approvals Granted
SLTApproved: Slta Waiver
Tested-by: Jira Key
Reviewed-by: Xiaojun Ji <jixj@motorola.com>
Reviewed-by: Huosheng Liao <liaohs@motorola.com>
Submit-Approved: Huosheng Liao <liaohs@motorola.com>
Tested-by: Huosheng Liao <liaohs@motorola.com>

storaged/storaged.cpp
@@ -192,9 +192,15 @@ void storaged_t::init_battery_service() {
 void storaged_t::binderDied(const wp<IBinder>& who) {
     if (battery_properties != NULL &&
         IInterface::asBinder(battery_properties) == who) {
-        LOG_TO(SYSTEM, ERROR) << "batteryproperties service died, exiting";
-        IPCThreadState::self()->stopProcess();
-        exit(1);
+        char property[256] = {0};
+        property_get("sys.powerctl", property,"");
+        if (strlen(property) != 0 ) {
+            LOG_TO(SYSTEM, ERROR) << "sys.powerctl = "<<property;
+        } else {
+            LOG_TO(SYSTEM, ERROR) << "batteryproperties service died, exiting";
+            IPCThreadState::self()->stopProcess();
+            exit(1);
+        }
     } else {
         LOG_TO(SYSTEM, ERROR) << "unknown service died";
     }

TheXPerienceProject/android_system_core

storaged/storaged.cpp
@@ -125,9 +125,15 @@ void storaged_t::init_health_service() {
 
 void storaged_t::serviceDied(uint64_t cookie, const wp<::android::hidl::base::V1_0::IBase>& who) {
     if (health != NULL && interfacesEqual(health, who.promote())) {
-        LOG_TO(SYSTEM, ERROR) << "health service died, exiting";
-        android::hardware::IPCThreadState::self()->stopProcess();
-        exit(1);
+        char property[256] = {0};
+        property_get("sys.powerctl", property,"");
+        if (strlen(property) != 0 ) {
+            LOG_TO(SYSTEM, ERROR) << "sys.powerctl = "<<property;
+        } else {
+            LOG_TO(SYSTEM, ERROR) << "health service died, exiting";
+            android::hardware::IPCThreadState::self()->stopProcess();
+            exit(1);
+        }
     } else {
         LOG_TO(SYSTEM, ERROR) << "unknown service died";
     }