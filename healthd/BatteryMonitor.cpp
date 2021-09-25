472ac7c5e0d0155f31456a3e1d48ec64c484543d
klozz <klozz@thexperienceproject.org>
2021-09-25 22:43:09 +0200

    healthd: Add WARP and VOOC charger type
    adapted to T
    
    Signed-off-by: klozz <klozz@thexperienceproject.org>
    Change-Id: I935e297b070ae76a069fa15b9910c9bfcebae8d5
    Signed-off-by: klozz <carlosj@klozz.dev>

--- healthd/BatteryMonitor.cpp
+++ healthd/BatteryMonitor.cpp
@@ -305,6 +305,8 @@ static BatteryMonitor::PowerSupplyType readPowerSupplyType(const String8& path) 
             {"Wireless", BatteryMonitor::ANDROID_POWER_SUPPLY_TYPE_WIRELESS},
             {"Dock", BatteryMonitor::ANDROID_POWER_SUPPLY_TYPE_DOCK},
             {"DASH", BatteryMonitor::ANDROID_POWER_SUPPLY_TYPE_AC},
+            {"VOOC", BatteryMonitor::ANDROID_POWER_SUPPLY_TYPE_AC},
+            {"WARP", BatteryMonitor::ANDROID_POWER_SUPPLY_TYPE_AC},
             {NULL, 0},
     };
     std::string buf;
